ccccae0ab908008d05d34f067c551b39b53588f6
kalhollywood <kal.hollywood@gmail.com>
2022-10-04 11:57:40 +0100

    basic discover page tiles complete

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+import { shazamCoreApi } from './services/shazamCore';
 import playerReducer from './features/playerSlice';
 
+// Boilerplate redux toolkit code.
 export const store = configureStore({
   reducer: {
+    [shazamCoreApi.reducerPath]: shazamCoreApi.reducer,
     player: playerReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(shazamCoreApi.middleware),
 });
